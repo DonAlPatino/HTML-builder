341a3d2ca5a8dfa8c5c1bc43bdd2caeaee81b524
DonAlPatino <valp@inbox.ru>
2023-05-02 12:46:40 +0300

    feat: task 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,8 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-
 const getFilePath = (filename) => path.join(__dirname, filename);
-
 const readableStream = fs.createReadStream(getFilePath('text.txt'), 'utf8');
 readableStream.on('data', (chunk) => {
   console.log(chunk);
