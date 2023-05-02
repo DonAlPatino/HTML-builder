eba8eb593a7287c62d52522741412b54fccc5723
DonAlPatino <valp@inbox.ru>
2023-05-02 13:21:29 +0300

    refactor: task 01-read-file simplify

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
+const { stdout } = process;
 const getFilePath = (filename) => path.join(__dirname, filename);
 const readableStream = fs.createReadStream(getFilePath('text.txt'), 'utf8');
-readableStream.on('data', (chunk) => {
-  console.log(chunk);
-});
+readableStream.on('data', data => stdout.write(data));
